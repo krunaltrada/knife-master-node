49941ab1b3376637bf887354e677166ae528e099
krunaltrada <krunaltrada1212@gmail.com>
2023-03-05 15:54:35 +0530

    asd

--- app.js
+++ app.js
@@ -9,21 +9,23 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const app = express();
 
+app.use(cors({ origin: "*" }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors({ origin: "*" }));
+app.use(express.static("./client"));
+app.get("/game", (req, res) => {
+  res.sendFile(path.resolve("./client/Build", "index.html"));
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("./client"));
-app.get("/game", (req, res) => {
-  res.sendFile(path.resolve("./client/Build", "index.html"));
-});
 
 // Support cross origin request
 // app.use(function (req, res, next) {

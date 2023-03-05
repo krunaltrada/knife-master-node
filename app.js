18b81581e1cc7bf2a07056224aed317ae66e4e89
krunaltrada <krunaltrada1212@gmail.com>
2023-03-05 14:55:27 +0530

    Cors Error Fix

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
